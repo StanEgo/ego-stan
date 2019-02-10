2dd261a23ae374b892cd001cb1301b26b2c5a903
stan <stanislav@yarmonov.pro>
2019-02-10 21:55:45 +0200

    *IEntity contract slightly updated.

--- labs/SDD/Frame/Model/IEntity.cs
+++ labs/SDD/Frame/Model/IEntity.cs
@@ -3,12 +3,13 @@ namespace SDD.Frame.Model
     /// <summary>
     /// Marker interface that class relates to <typeparamref name="T"/> entity.
     /// </summary>
+    /// 
     /// <remarks>
     /// We may have a lot of classes supporting same entity. Partitions,
     /// view-models, CRUD-related classes, etc. Using this interface we can
     /// align all of them.
     /// </remarks>
-    public interface IEntity<T>
+    public interface IEntity<TEntity>
     {
         
     }
