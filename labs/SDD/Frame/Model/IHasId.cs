912a67f63e21fc24a428f0ad8058a32c47a958a3
stan <stanislav@yarmonov.pro>
2019-02-10 21:55:10 +0200

    +Added IHasId interface.

---
+++ labs/SDD/Frame/Model/IHasId.cs
@@ -0,0 +1,15 @@
+﻿namespace SDD.Frame.Model
+{
+    /// <summary>
+    /// This interface defines identified entities by using a
+    /// <see cref="Id"/> property.
+    /// </summary>
+    /// 
+    /// <typeparam name="TKey">
+    /// Type of the <see cref="Id"/>.
+    /// </typeparam>
+    public interface IHasId<TKey>
+    {
+        TKey Id { get; set; }
+    }
+}
