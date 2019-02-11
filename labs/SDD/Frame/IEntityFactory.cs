34a21bdbe92728cdb4f499f6f8690d9edbc2e043
stan <stanislav@yarmonov.pro>
2019-02-11 22:39:41 +0200

    +Added IEntityFactory contract.

---
+++ labs/SDD/Frame/IEntityFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace SDD.Frame
+{
+    /// <summary>
+    /// Create default implementations for selected.
+    /// <typeparamref name="TEntity"/>.
+    /// </summary>
+    public interface IEntityFactory<TEntity>
+    {
+        /// <summary>
+        /// Create instance of <typeparamref name="TEntity"/>.
+        /// </summary>
+        TEntity Create();
+    }
+}
