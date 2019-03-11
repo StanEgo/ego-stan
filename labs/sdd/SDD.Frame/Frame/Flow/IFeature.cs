c06320cd7b693aea301e439998324543a9ce0b6a
stan <stanislav@yarmonov.pro>
2019-03-11 23:13:04 +0200

    +Added IFeature idea.

---
+++ labs/sdd/SDD.Frame/Frame/Flow/IFeature.cs
@@ -0,0 +1,15 @@
+﻿namespace SDD.Frame.Flow
+{
+    /// <summary>
+    /// A capability of getting a <typeparamref name="TProduct"/>
+    /// from specific <typeparamref name="TInput"/>.
+    /// </summary>
+    public interface IFeature<TInput, TProduct>
+    {
+        /// <summary>
+        /// Get a <typeparamref name="TProduct"/> from provided
+        /// <paramref name="input"/>.
+        /// </summary>
+        TProduct Get(TInput input);
+    }
+}
---
+++ labs/sdd/SDD.Frame/Frame/Flow/NamespaceDoc.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SDD.Frame.Flow
+{
+    /// <summary>
+    /// Basic flow operations.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// Technically any application is a complex sequence of input/output
+    /// transformations. I can easily imagine this using functional paradigm -
+    /// output = f(input). So this namespace is build on <see cref="IFactory"/>
+    /// responsible for creating the initial input and
+    /// <see cref="IFeature{TInput, TProduct}"/> that transforms from input
+    /// to product.
+    /// </remarks>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+
+    }
+}
