d33c936dc1e7f1be90da00a993bf48ce5ae6ad66
stan <stanislav@yarmonov.pro>
2019-03-12 07:40:30 +0200

    +Added point to extend application composition.

---
+++ labs/sdd/SDD.Frame/Application/Composition/ApplicationComposition.cs
@@ -0,0 +1,17 @@
+﻿using SDD.Security;
+
+namespace SDD.Application.Composition
+{
+    /// <summary>
+    /// TODO:Attempt to define basic application skeleton.
+    /// </summary>
+    public class ApplicationComposition : IComposition
+    {
+        public ISecurityComposition Security { get; }
+
+        public ApplicationComposition(ISecurityComposition security)
+        {
+            Security = security;
+        }
+    }
+}
