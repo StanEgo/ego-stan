603dad4f3cdad0cb6acdef0533a5617281de7954
stan <stanislav@yarmonov.pro>
2019-03-12 16:58:52 +0200

    +Added some composition ideas.

--- labs/sdd/SDD.Frame/Application/Composition/IComposition.cs
+++ labs/sdd/SDD.Frame/Application/Composition/IComposition.cs
@@ -1,5 +1,13 @@
 ﻿namespace SDD.Application.Composition
 {
+    /// <todo>Define responsibility</todo>
+    /// 
+    /// <todo>
+    /// Composition can work with/define a context/frame. We used to such
+    /// frames when working with routing in web, e.g. "/account/get" or CLI
+    /// apps "sdd account get". And such context can define such names as
+    /// well useable for routes/CLI commands/etc.
+    /// </todo>
     public interface IComposition
     {
 
