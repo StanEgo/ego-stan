4d66365904f0964c02fb2fa259476283983bdab6
stan <stanislav@yarmonov.pro>
2019-07-03 07:57:19 +0300

    +Added sql module for Rust sample.

---
+++ labs/sdd/code/rust/crates/cloud-cli/src/db/sql.rs
@@ -0,0 +1,47 @@
+use std::fmt;
+use futures::{ Future };
+use tokio_postgres::{ Client };
+
+pub struct SqlConnection<'a> {
+    pub server: &'a str,
+    pub port: u16,
+    pub database: &'a str,
+    pub user: &'a str,
+    pub password: &'a str
+}
+
+impl<'a> fmt::Display for SqlConnection<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "server = {} port = {} database = {} user = {} password = {}",
+            self.server,
+            self.port,
+            self.database,
+            self.user,
+            self.password
+        )
+    }
+}
+
+pub const DEFAULT_PG: SqlConnection = SqlConnection {
+    server: "localhost",
+    port: 5432,
+    database: "postgres",
+    user: "postgres",
+    password: ""
+};
+
+pub struct SqlPipeline {
+    client: Client
+}
+
+pub trait SqlPipe {
+    fn execute() -> Future<Item = SqlPipeline, Error = ()>;
+}
+
+// impl SqlPipe for SqlConnection {
+//     fn execute() -> Future<Item = SqlPipeline, Error = ()> {
+//         unimplemented!();
+//     }
+// }
