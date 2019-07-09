059a6a4352daa4e0bc7d326df521e907edbd10ce
stan <stanislav@yarmonov.pro>
2019-07-09 07:58:42 +0300

    +RDB Mesh password.

---
+++ labs/rdb-mesh/0001/security/password/table.sql
@@ -0,0 +1,12 @@
+/*
+  
+	Authentication based on password.
+
+*/
+CREATE TABLE security.password (
+	id mesh.id NOT NULL,
+		CONSTRAINT pk_security_password PRIMARY KEY(id),
+		CONSTRAINT fk_security_password_id FOREIGN KEY (id) REFERENCES mesh.entity(id),
+
+		hash bytea NOT NULL
+) WITHOUT OIDS;
--- labs/rdb-mesh/0001/security/schema.sql
+++ labs/rdb-mesh/0001/security/schema.sql
@@ -2,3 +2,4 @@ CREATE SCHEMA security;
 
 \i 0001/security/space/table.sql
 \i 0001/security/session/table.sql
+\i 0001/security/password/table.sql
