b326ce6e20e1a341f2da7b31df1a39f9647cc835
stan <stanislav@yarmonov.pro>
2019-09-16 14:41:46 +0300

    *EGO notes.

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,31 +1,34 @@
 import "../styles/index.scss";
 
-const styles = {
-    columnCount: 3
-}
-
 const Home = () => (
     <div>
         <h1>EGO философия</h1>
 
-        <div style={ styles }>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
-            aut, quos animi cumque optio illo saepe necessitatibus eveniet
-            doloribus quis sequi tenetur illum. Asperiores, nihil distinctio
-            sequi excepturi repellendus soluta. Lorem ipsum dolor sit, amet
-            consectetur adipisicing elit. Delectus, reprehenderit repellat.
-            Tenetur repudiandae beatae libero error adipisci quidem exercitationem
-            minima ex nam tempora rem qui maxime repellat corrupti illo, non
-            aspernatur omnis commodi sit quis quibusdam. Id aperiam officiis
-            commodi autem doloribus fugit earum doloremque veniam a quam iure
-            cupiditate, corporis repellendus corrupti hic incidunt, quod
-            voluptate. Placeat reprehenderit, explicabo et dolore sed dicta
-            expedita, saepe, numquam reiciendis exercitationem mollitia
-            architecto quo. Totam officiis hic pariatur corporis facere beatae
-            aspernatur aliquam rem. Nemo quo, cum aliquid reiciendis tenetur
-            totam veritatis accusamus quas minus ea culpa architecto voluptates
-            provident recusandae quis!
-        </div>
+        <h2 className="whale">Excito</h2>
+        <ul>
+            <li>
+                Когда мы думаем, что нам нечего делать, на самом деле нам просто не
+                хочется ничего делать. Самих возможностей - огромное многообразие.
+            </li>
+            <li>
+                Привычные индикаторы - прокрастинация, мотивация.
+            </li>
+            <li>
+                Excito &ndash; это то, что наполняет нашу жизнь.
+            </li>
+            <li>
+                Excito &ndash; как огонь, стоит разжечь и уже не остановить.
+            </li>
+        </ul>
+
+        <h2>В фитнесе</h2>
+        <ul>
+            <li>
+                Высокоинтенсивный тренинг (в том числе интервальный, HIIT).
+                Интенсивность &ndash; даёт Excito. В основном на гормональном
+                уровне.
+            </li>
+        </ul>
     </div>
 )
 
